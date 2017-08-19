f88b645dcaf4da52049f720e8725ed36418b7e16
范政杰 <fanzhengjie@u51.com>
2017-08-19 09:35:30 +0800

    项目初始化

---
+++ src/main/java/com/jary/daily/GrowingApplication.java
@@ -0,0 +1,12 @@
+package com.jary.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingApplication.class, args);
+	}
+}
