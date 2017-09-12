89623aeb66695a79eff54e5048d32759cc9ebd89
范政杰 <fanzhengjie@u51.com>
2017-09-12 14:26:34 +0800

    如何获取emp_v和employees有相同的数据no

--- src/main/java/com/jary/daily/test/Test170828.java
+++ src/main/java/com/jary/daily/test/Test170828.java
@@ -1,8 +1,5 @@
 package com.jary.daily.test;
 
-import java.math.BigDecimal;
-import java.util.*;
-
 /**
  * @author fanzhengjie
  * @version 1.0
@@ -10,26 +7,13 @@ import java.util.*;
  */
 public class Test170828 {
 
-    public static void main(String[] args){
-       /* ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        Iterator<Integer> iterator = list.iterator();
-        while(iterator.hasNext()){
-            Integer integer = iterator.next();
-            if(integer == 10)
-                list.remove(10);
-        }*/
 
-       ArrayList<String> list = new ArrayList<>();
-       for(int i=0;i<10;i++){
-           list.add("sh"+i);
-       }
-       for(int i=0;list.iterator().hasNext();i++){
-           list.remove(i);
-           System.out.println("result"+list.get(i));
-       }
+    public static void main(String[] args) {
+        int k = 1 ^ (1 << 31 >> 31);
+        System.out.println(k);
+    }
 
-
-
+    public static void print(int num) {
+        System.out.println(Integer.toBinaryString(num));
     }
 }
