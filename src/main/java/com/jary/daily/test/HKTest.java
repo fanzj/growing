f54510eb8b042a27072dcc7c1581c93e819c8206
范政杰 <fanzhengjie@u51.com>
2017-09-15 19:41:34 +0800

    二叉查找树

---
+++ src/main/java/com/jary/daily/test/HKTest.java
@@ -0,0 +1,21 @@
+package com.jary.daily.test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author fanzhengjie
+ * @version 1.0
+ * @create 2017/9/14 下午2:41
+ */
+public class HKTest {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+
+    }
+}
---
+++ src/main/java/com/jary/daily/test/Main.java
@@ -0,0 +1,14 @@
+package com.jary.daily.test;
+
+/**
+ * @author fanzhengjie
+ * @version 1.0
+ * @create 2017/9/14 下午7:02
+ */
+public class Main {
+
+    public static void main(String[] args){
+        int a = 10;
+        System.out.println("a="+((a<10)?9.9:10));
+    }
+}
