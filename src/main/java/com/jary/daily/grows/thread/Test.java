07b58936f4d357432265a08976f5eaf8d27e4b1b
范政杰 <fanzhengjie@u51.com>
2017-10-07 20:47:34 +0800

    4种线程池

--- src/main/java/com/jary/daily/grows/thread/Test.java
+++ src/main/java/com/jary/daily/grows/thread/Test.java
@@ -2,6 +2,8 @@ package com.jary.daily.grows.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -22,16 +24,97 @@ public class Test {
          */
         @Override
         public void uncaughtException(Thread t, Throwable e) {
-            System.out.println("This is:"+t.getName()+", Message:"+e.getMessage());
+            System.out.println("This is:" + t.getName() + ", Message:" + e.getMessage());
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        ExecutorService service = Executors.newCachedThreadPool();
-        service = Executors.newFixedThreadPool(3);
         Thread thread = null;
         Lock lock = new ReentrantLock();
 
+
+        test4();
+
+    }
+
+    private void viewSourceCode() {
+        //通过Executors提供四种线程池
+        ExecutorService service1 = Executors.newCachedThreadPool();//1
+        ExecutorService service2 = Executors.newFixedThreadPool(3);//2
+        ExecutorService service3 = Executors.newScheduledThreadPool(3);//3
+        ExecutorService service4 = Executors.newSingleThreadExecutor();//4
+    }
+
+    private static void test1() {
+        ExecutorService service = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(index * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(index);
+                }
+            });
+        }
+        service.shutdown();
+    }
+
+    private static void test2() {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(index);
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        service.shutdown();
+    }
+
+    private static void test3() {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(5);
+        service.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("delay 3 seconds ");
+            }
+        }, 3, TimeUnit.SECONDS);
+
+       /* ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                System.out.println("delay 1 seconds, and excute every 3 seconds"+Thread.currentThread().getName());
+            }
+        }, 1, 3, TimeUnit.SECONDS);*/
+    }
+
+    public static void test4(){
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            singleThreadExecutor.execute(new Runnable() {
+                public void run() {
+                    try {
+                        System.out.println(index);
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 }
