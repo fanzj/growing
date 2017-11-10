e7fc6b25f205c2c4024c2b9e21f5ca8ac2fb17ba
范政杰 <fanzhengjie@u51.com>
2017-11-10 10:20:15 +0800

    道富机试题-门禁

--- src/main/java/com/jary/daily/grows/java8/LambdaDemo.java
+++ src/main/java/com/jary/daily/grows/java8/LambdaDemo.java
@@ -1,8 +1,14 @@
 package com.jary.daily.grows.java8;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author fanzhengjie
@@ -11,7 +17,18 @@ import java.util.List;
  */
 public class LambdaDemo {
 
-    public static void main(String[] args){
+    private static String readFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        StringBuilder builder = new StringBuilder();
+        if(file.exists()){
+            System.out.println("file existed");
+            Path path = file.toPath();
+            Files.lines(path, StandardCharsets.UTF_8).forEach(builder::append);
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
         /*Arrays.asList("a","b","d").forEach(e -> System.out.println(e));
         Arrays.asList("a","b","d").forEach((String e) -> System.out.println(e));
         Arrays.asList("a","b","d").forEach(e -> {
@@ -22,12 +39,15 @@ public class LambdaDemo {
         list.sort((e1,e2) -> e1.compareTo(e2));
         System.out.println(list);*/
 
-        List<String> list =  Arrays.asList("d","b","a","c");
+        /*List<String> list =  Arrays.asList("d","b","a","c");
         list.sort((e1,e2) -> {
             int result = e1.compareTo(e2);
             return result;
         });
-        System.out.println(list);
+        System.out.println(list);*/
+
+        String path = "/Users/fanzhengjie/growing/src/main/resources/config/maintain.json";
+        System.out.println(readFile(path));
     }
 
 }
---
+++ src/main/java/com/jary/daily/grows/test/DaoFuDemo.java
@@ -0,0 +1,103 @@
+package com.jary.daily.grows.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author fanzhengjie
+ * @version 1.0
+ * @create 2017/11/10 上午8:57
+ */
+public class DaoFuDemo {
+
+    public static final int MAX_ID = 1002;
+
+    static class Employee {
+
+        List<Integer> in;//进入时间列表
+        List<Integer> out;//离开时间列表
+        List<Integer> diff;//滞留时间
+        int total_stay;//该员工的最大滞留时间
+
+        public Employee() {
+            in = new ArrayList<>();
+            out = new ArrayList<>();
+            diff = new ArrayList<>();
+            total_stay = 0;
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+        Employee[] employees = new Employee[MAX_ID];//每个索引表示员工编号
+        int N;//N条记录
+
+        N = Integer.valueOf(in.nextLine());
+        for (int i = 1; i <= N; i++) {
+            String line = in.nextLine();//输入的每行数据
+            String[] lines = line.split(" ");
+            Integer id = Integer.valueOf(lines[0]);//每条记录的编号
+            String tag = lines[1];//进入或离开
+            String[] str_time = lines[2].split(":");
+            int[] time = new int[2];
+            time[0] = Integer.valueOf(str_time[0]);//时
+            time[1] = Integer.valueOf(str_time[1]);//分
+            int total_min = time[0] * 60 + time[1];
+
+            //作为结构输入
+            employees[id] = new Employee();
+            if ("E".equals(tag)) {//进入
+                employees[id].in.add(total_min);
+            } else if ("L".equals(tag)) {//离开
+                employees[id].out.add(total_min);
+            }
+        }
+
+        int total_num = 0;//有效记录数
+        int total_time = 0;//总时间数
+        int max_time = Integer.MIN_VALUE;//最大时间
+        for (int i = 1; i <= MAX_ID; i++) {
+            //筛选有效记录
+            if(employees[i]!=null){
+                int size_in = employees[i].in.size();//员工i进入的时间列表长度
+                int size_out = employees[i].out.size();//员工i离开时时间列表长度
+                if (size_in != 0 && size_out != 0) {//有效记录
+                    int min_size = Math.min(size_in, size_out);
+
+                    //记录有效，计算每次进入离开的滞留时间
+                    int total_stay = 0;
+                    for (int j = 0; j < min_size; j++) {
+                        employees[i].diff.add(employees[i].out.get(j) - employees[i].in.get(j));
+
+                        //该员工的总滞留时间
+                        total_stay += employees[i].out.get(j) - employees[i].in.get(j);
+
+                        //总时间增加
+                        total_time += employees[i].out.get(j) - employees[i].in.get(j);
+
+
+                    }
+
+                    employees[i].total_stay = total_stay;
+
+                    //找出最大滞留时间
+                    max_time = Math.max(max_time, employees[i].total_stay);
+
+                    //有效记录增加
+                    total_num++;
+
+                }
+
+            }
+
+        }
+
+        System.out.printf("avg=%d\n", total_time / total_num);
+        System.out.printf("max=%d\n", max_time);
+
+
+    }
+
+}
