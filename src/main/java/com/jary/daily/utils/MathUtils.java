b4dc22c2e2fc64fa27fa7e130ccc9645a19811c5
范政杰 <fanzhengjie@u51.com>
2017-11-24 16:51:06 +0800

    添加工具类

---
+++ src/main/java/com/jary/daily/utils/MathUtils.java
@@ -0,0 +1,10 @@
+package com.jary.daily.utils;
+
+/**
+ * @author fanzhengjie
+ * @version 1.0
+ * @create 2017/11/24 下午3:08
+ */
+public class MathUtils {
+
+}
---
+++ src/main/java/com/jary/daily/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.jary.daily.utils;
+
+/**
+ * @author fanzhengjie
+ * @version 1.0
+ * @create 2017/11/24 上午11:18
+ */
+public class StringUtils {
+
+}
